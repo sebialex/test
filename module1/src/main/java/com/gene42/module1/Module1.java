77d942a889dcf8d4f08c2b9d599c15f18a25c9a1
Sebastian Stanisor <sebialex@gmail.com>
2016-09-29 16:51:41 -0400

    update

---
+++ module1/src/main/java/com/gene42/module1/Module1.java
@@ -0,0 +1,16 @@
+package com.gene42.module1;
+
+/**
+ * DESCRIPTION.
+ *
+ * @version $Id$
+ */
+public class Module1
+{
+    public static void main(String ... args)
+    {
+
+       System.out.println("Module1");
+
+    }
+}
